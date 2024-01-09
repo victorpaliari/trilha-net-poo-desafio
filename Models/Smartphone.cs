fc5d85d5541054824b0aaa1a72a2cc2ef579cd75
Victor Paliari <victorrpaliari@gmail.com>
2024-01-09 18:50:33 -0300

    Desafio POO Concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,10 +4,17 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo {get;}
 
-        public Smartphone(string numero)
+        private string Imei {get;}
+
+        private int Memoria{get;}
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("1234", "AASD", "159", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("7898", "TYUI", "489", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("WhatsApp");
